c9bd8f08d5d9167647cb36743f129f7271e0da41
minhbiken <minhpc.lucius@gmail.com>
2023-08-16 15:58:00 +0800

    modify movie api

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('homepage', 'Api\HomepageController')->only(['index']);
-Route::apiResource('movies', 'Api\MovieController')->only(['index', 'show']);
+Route::apiResource('movies', 'Api\MovieController')->only(['index']);
 Route::apiResource('tvshows', 'Api\TvshowController')->only(['index']);
