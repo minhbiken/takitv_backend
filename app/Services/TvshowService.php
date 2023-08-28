8bcec2fe12a305d672b31c6bf155eb662bbb77bc
vvc19121990 <vvc19121990@yahoo.com>
2023-08-28 11:21:00 +0700

    modify top month

--- app/Services/TvshowService.php
+++ app/Services/TvshowService.php
@@ -31,7 +31,7 @@ class TvshowService {
         } else {
             $queryByType =  "AND t.slug = '" . $type . "'" ;
         }
-        $queryTopWeek = "SELECT DISTINCT(p.ID) as get_not_exist, p.ID, p.post_title, p.original_title, p.post_content, p.post_date_gmt, mp.7_day_stats FROM wp_posts p
+        $queryTopWeek = "SELECT DISTINCT(p.ID) as get_not_exist, p.ID, p.post_title, p.original_title, p.post_content, p.post_date_gmt, mp.30_day_stats FROM wp_posts p
                         LEFT JOIN wp_most_popular mp ON p.ID = mp.post_id
                         LEFT JOIN wp_term_relationships tr ON tr.object_id = mp.post_id
                         LEFT JOIN wp_term_taxonomy tx on tr.term_taxonomy_id = tx.term_taxonomy_id
