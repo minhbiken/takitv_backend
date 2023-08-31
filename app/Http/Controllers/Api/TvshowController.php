e41cf40c5bbae87dd21e8cad206d5b16b26abea2
vvc19121990 <vvc19121990@yahoo.com>
2023-08-31 09:51:08 +0700

    get topweek for action adventure

--- app/Http/Controllers/Api/TvshowController.php
+++ app/Http/Controllers/Api/TvshowController.php
@@ -67,11 +67,18 @@ class TvshowController extends Controller
                 $genre[$key] = "'" . "$g" . "'";
             }
             $genre = join(",", $genre);
-          
-            $queryGenre = "SELECT tr.object_id FROM wp_terms t
+
+            if( $genre == "'action-adventure'") {
+                $queryGenre = "SELECT tr.object_id FROM wp_terms t
+                left join wp_term_taxonomy tx on tx.term_id = t.term_id
+                left join wp_term_relationships tr on tr.term_taxonomy_id = tx.term_taxonomy_id
+                WHERE t.slug IN (". $genre .") ";
+            } else {
+                $queryGenre = "SELECT tr.object_id FROM wp_terms t
                 left join wp_term_taxonomy tx on tx.term_id = t.term_id
                 left join wp_term_relationships tr on tr.term_taxonomy_id = tx.term_taxonomy_id
                 WHERE t.name IN (". $genre .") ";
+            }
             $where = $where . "AND p.ID IN ( ". $queryGenre ." ) ";    
         }
 
