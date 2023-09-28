222588cde06baa8ecf0963264a9a7248fcd45ee9
bikencms <minhbiken14@gmail.com>
2023-09-28 22:45:20 +0700

    add model post, post meta

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Post extends Model
+{
+    protected $table = 'wp_posts';
+    protected $fillable = [
+        'post_title', 
+        'post_content', 
+        'post_status',
+        'guid', 
+        'post_type', 
+        'post_excerpt', 
+        'to_ping', 
+        'pinged',
+        'post_content_filtered',
+        'post_date',
+        'post_date_gmt',
+        'post_modified',
+        'post_modified_gmt'
+    ];
+}
---
+++ app/Models/PostMeta.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class PostMeta extends Model
+{
+    protected $table = 'wp_postmeta';
+    protected $primaryKey = 'meta_id';
+    protected $fillable = [
+        'post_id', 
+        'meta_key',
+        'meta_value', 
+    ];
+}
