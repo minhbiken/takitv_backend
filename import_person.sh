059130c75e106c4323850ee223a593777ead329f
vvc19121990 <vvc19121990@yahoo.com>
2023-10-03 15:51:22 +0700

    modify import_person.sh

--- import_person.sh
+++ import_person.sh
@@ -2,3 +2,14 @@
 echo "post_id = $1 "
 echo "tmdb_id = $2 "
 echo "type = $3 "
+
+if [[ $3 == 'movie' ]]
+then
+  echo "Run movie.js $1 $2"
+  cd /home/vuejs/backend && node ./../crawler_person/movie.js $1 $2
+else
+  echo "Run tv_show.js $1 $2"
+  cd /home/vuejs/backend && node ./../crawler_person/tv_show.js $1 $2
+fi
+
+ cd /home/vuejs/backend && php artisan person:insert data.json $3
