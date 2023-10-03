41001579c10cf24be14d9aa949a62997733d6717
root <root@uc-blade24-4.maas>
2023-10-03 09:35:14 +0000

    modify bash import person

--- import_person.sh
+++ import_person.sh
@@ -6,10 +6,10 @@ echo "type = $3 "
 if [[ $3 == 'movie' ]]
 then
   echo "Run movie.js $1 $2"
-  cd /home/vuejs/backend && node ./../crawler_person/movie.js $1 $2
+  cd /home/vuejs/crawler_person && node movie.js $1 $2
 else
   echo "Run tv_show.js $1 $2"
-  cd /home/vuejs/backend && node ./../crawler_person/tv_show.js $1 $2
+  cd /home/vuejs/crawler_person && node tv_show.js $1 $2
 fi
 
 cd /home/vuejs/backend && php artisan person:insert data.json $3
