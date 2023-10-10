a995f7c6966c0f0ec1c52d1649f3501f768db4d4
vvc19121990 <vvc19121990@yahoo.com>
2023-10-10 14:24:58 +0700

    modify api person detail

--- app/Http/Controllers/Api/CastController.php
+++ app/Http/Controllers/Api/CastController.php
@@ -140,7 +140,7 @@ class CastController extends Controller
                     $select = "SELECT p.ID, p.post_title, p.post_name, p.original_title, p.post_content, p.post_date_gmt, p.post_date, p.post_modified 
                     FROM wp_posts p 
                     WHERE  ((p.post_type = 'tv_show' AND (p.post_status = 'publish'))) AND p.ID=". $tvShowId;
-                    $tvShowData[] = $this->tvshowService->getItems($select);
+                    $tvShowData[] = $this->tvshowService->getItems($select)[0];
                 }
             }
             $cast['tv_show'] = $tvShowData;
@@ -153,7 +153,7 @@ class CastController extends Controller
                     $select = "SELECT p.ID, p.post_title, p.post_name, p.original_title, p.post_content, p.post_date_gmt, p.post_date, p.post_modified 
                     FROM wp_posts p 
                     WHERE  ((p.post_type = 'movie' AND (p.post_status = 'publish'))) AND p.ID=". $movieId;
-                    $movie[] = $this->movieService->getItems($select);
+                    $movie[] = $this->movieService->getItems($select)[0];
                 }
             }
             $cast['movie'] = $movie;
