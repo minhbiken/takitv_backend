7393e37ecf1d61fe63917311b923883e2fb88bc3
vvc19121990 <vvc19121990@yahoo.com>
2023-10-11 09:38:12 +0700

    modify tv show detail

--- app/Http/Controllers/Api/TvshowController.php
+++ app/Http/Controllers/Api/TvshowController.php
@@ -141,7 +141,7 @@ class TvshowController extends Controller
         $titleTvshow = $request->get('title', '');
         $select = "SELECT p.ID, p.post_title, p.original_title, p.post_content, p.post_date_gmt, p.post_date, p.post_modified FROM wp_posts p ";
         $where = " WHERE  ((p.post_type = 'tv_show' AND (p.post_status = 'publish'))) ";
-        $whereTitle = " AND p.post_title='". $titleTvshow ."'  LIMIT 1; ";
+        $whereTitle = " AND (p.post_title='". $titleTvshow ."' OR p.post_name='". $titleTvshow ."' )  LIMIT 1; ";
 
         $where = $where . $whereTitle;
         $movies = [];
