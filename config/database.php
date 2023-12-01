8532ecd59d91458d96e43aaf79e3d2b5bd4f5b36
DESKTOP-UA509KQ\ADMIN <dev4.vuiad.@gmail.com>
2023-12-01 15:07:17 +0700

    update cache key

--- config/database.php
+++ config/database.php
@@ -124,8 +124,7 @@ return [
         'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'cluster' => env('REDIS_CLUSTER', 'redis')
         ],
 
         // 'default' => [
